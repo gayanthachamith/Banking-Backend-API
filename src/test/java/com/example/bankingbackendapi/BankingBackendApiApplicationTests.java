60a24b131035a30a2ac149349da4eb7f6b7aab79
Gayantha Chamith <73245735+gayanthachamith@users.noreply.github.com>
2026-03-29 10:31:05 +0300

    Task 01
    Main initialization

---
+++ src/test/java/com/example/bankingbackendapi/BankingBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
